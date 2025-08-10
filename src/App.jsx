eca29cb6980feb7da65c8ccab168619d12af950e
Larissa Hirai <larissa.hirai@mail.uft.edu.br>
2025-08-10 20:48:57 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import ButtonLanguageSwitch from "./components/buttonLanguageSwitch.jsx";
 import Hero from "./components/hero.jsx";
 import Navbar from "./components/navbar.jsx";
